cd9e711ace7873a6059023641a597d9b3a112a56
peixed351 <peixed351@gmail.com>
2024-01-02 16:44:16 +0300

    update image id

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://dacl.ai/workshop.html"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 11899225
+PREVIEW_IMAGE_ID: int = 11909145
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/dacl10k"
